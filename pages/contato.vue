a01dbd0042a8e24a41154bc037692916578b598d
Rafael Alves <rafalves.costa@gmail.com>
2023-09-16 16:24:59 -0300

    added schema.org

--- pages/contato.vue
+++ pages/contato.vue
@@ -4,16 +4,6 @@
 
 
 <script setup>
-useSchemaOrg([
-  defineOrganization({
-    name: 'Melhores Compras Online',
-    logo: '/img/banner.jpg',
-    inLanguage: 'pt_BR',
-    tagPosition: 'head',
-    url: 'https://melhores-compras.online',
-    description: 'Melhores Compras Online | Análise, Promoção e Informação de Produtos Encontrados na Internet',
-  })
-])
 
 
 </script>
